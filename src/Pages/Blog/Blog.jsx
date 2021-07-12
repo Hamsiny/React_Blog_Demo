359a7f5c9b5f3fbee051c2e28236c3d120ae05d4
曲洪澎 <hamsiny@quhongpengdeMacBook-Pro.local>
2021-07-12 14:38:34 +1200

    fetch and show data in blog comp

--- src/Pages/Blog/Blog.jsx
+++ src/Pages/Blog/Blog.jsx
@@ -1,8 +1,8 @@
-import React from 'react'
+import React, {useState, useEffect} from 'react'
 import './Blog.css'
 
 const Blog = () => {
-    const state = {data: ''};
+    const [data, setData] = useState([]);
 
     const fetchData = async () => {
         const res = await fetch('https://jsonplaceholder.typicode.com/posts');
@@ -10,21 +10,32 @@ const Blog = () => {
         return data;
     }
 
-    const getData = async () => {
-        const dataFromServer = await fetchData();
-        
-    }
+    useEffect(() => {
+        const getData = async () => {
+            const dataFromServer = await fetchData();
+            setData(dataFromServer);
+        }
+        getData();
+    }, []);
 
     return (
         <div>
-            <div className="container mt-4 mx-3">
+            <div className="container mt-4 mx-auto">
                 <h2 className="mb-3">Blog Posts</h2>
                 <div id="post" className="mr-3">
+                    {data.map(element => (
+                        <div key={element.id} className="mt-3 mr-3 dataitem">
+                            <h4>{element.id} - {element.title}</h4>
+                            <p>{element.body}</p>
+                            <hr/>
+                        </div>
+                    ))}
 
                 </div>
             </div>
         </div>
-    )
+    ); 
+
 }
 
 export default Blog
