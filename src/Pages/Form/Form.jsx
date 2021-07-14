1ef9a99cc3895affd61e45cf1cdfcf21b671af42
Hamsiny <459516309@qq.com>
2021-07-14 14:54:48 +1200

    update react hook form lib

--- src/Pages/Form/Form.jsx
+++ src/Pages/Form/Form.jsx
@@ -1,42 +1,43 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
+import { useForm } from 'react-hook-form'
 import './Form.css'
 
 const Form = () => {
+    const { register, handleSubmit } = useForm();
+    const onSubmit = data => console.log(data); 
+
     return (
         <>
             <div className="mt-5 wform">
                 <h3 className='formtitle'>Form</h3>
-                <form className='needs-validation mt-4 mx-5 mb-4'>
+                <form className='mt-4 mx-5 mb-4'>
                     <p><strong>Please fill your details:</strong></p>
                     <div className="form-group">
-                        <label for="validationCustom01">First Name</label>
+                        <label htmlFor="validationCustom01">First Name</label>
                         <input type="text" className="form-control" id="validationCustom01" placeholder="Your first name"/>
-                        <div className="valid-feedback">
-                            Looks good!
-                        </div>
                     </div>
                     <div className="form-group">
-                        <label for="exampleFormControlInput1">Last Name</label>
+                        <label htmlFor="exampleFormControlInput1">Last Name</label>
                         <input type="text" className="form-control" id="exampleFormControlInput1" placeholder="Your last name"/>
                     </div>
                     <div className="form-group">
-                        <label for="exampleFormControlInput1">Email address</label>
+                        <label htmlFor="exampleFormControlInput1">Email address</label>
                         <input type="email" className="form-control" id="exampleFormControlInput1" placeholder="name@example.com"/>
                     </div>
                     <div className="form-group">
-                        <label for="exampleFormControlInput1">Phone Number</label>
+                        <label htmlFor="exampleFormControlInput1">Phone Number</label>
                         <input type="phone" className="form-control" id="exampleFormControlInput1" placeholder="021 12345"/>
                     </div>
                     <div className="form-group">
-                        <label for="exampleFormControlInput1">Date of Birth</label>
+                        <label htmlFor="exampleFormControlInput1">Date of Birth</label>
                         <input type="date" className="form-control" id="exampleFormControlInput1"/>
                     </div>
                     <div className="form-group">
-                        <label for="exampleFormControlTextarea1">Comments</label>
+                        <label htmlFor="exampleFormControlTextarea1">Comments</label>
                         <textarea className="form-control" id="exampleFormControlTextarea1" rows="3" placeholder="Enter comments here"></textarea>
                     </div>
-                    <button className='btnn blue mr-3' type='submit'>Submit</button>
+                    <button id='submitbutton' className='btnn blue mr-3' type='submit'>Submit</button>
                     <Link to='/'><button className='btnn black'>Home</button></Link>
                 </form>
             </div>
