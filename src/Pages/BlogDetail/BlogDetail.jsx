6c3de5bddff5ea1189bcd75eba5a7eef3f69bcb7
Hamsiny <459516309@qq.com>
2021-07-14 22:39:21 +1200

    update validation

--- src/Pages/BlogDetail/BlogDetail.jsx
+++ src/Pages/BlogDetail/BlogDetail.jsx
@@ -1,10 +1,10 @@
-import React, {useState, useEffect} from 'react'
+import React, { useState, useEffect } from 'react'
 import { useParams } from 'react-router'
 import { Link } from 'react-router-dom';
 import './BlogDetail.css'
 
 const BlogDetail = () => {
-    const {id} = useParams();
+    const { id } = useParams();
     const [data, setData] = useState([]);
 
     const fetchData = async () => {
--- src/Pages/Form/Form.jsx
+++ src/Pages/Form/Form.jsx
@@ -3,45 +3,68 @@ import { Link } from 'react-router-dom'
 import { useForm } from 'react-hook-form'
 import './Form.css'
 
+// const FormValues = {
+//     firstName: String,
+//     lastName: String,
+//     email: String,
+//     phoneNumber: Number,
+//     dateOfBirth: Date,
+//     comments: String
+// };
+
 const Form = () => {
-    const { register, handleSubmit } = useForm();
-    const onSubmit = data => console.log(data); 
+    const { register, handleSubmit, formState: { errors } } = useForm();
+    const onSubmit = data => console.log(data);
 
     return (
         <>
             <div className="mt-5 wform">
                 <h3 className='formtitle'>Form</h3>
-                <form className='mt-4 mx-5 mb-4'>
+                <form className='mt-4 mx-5 mb-4' onSubmit={handleSubmit(onSubmit)}>
                     <p><strong>Please fill your details:</strong></p>
                     <div className="form-group">
-                        <label htmlFor="validationCustom01">First Name</label>
-                        <input type="text" className="form-control" id="validationCustom01" placeholder="Your first name"/>
+                        <label htmlFor="firstName">First Name</label>
+                        <input {...register("firstName", { required: true, maxLength: 20, pattern: /^[A-Za-z]+$/i })} type="text" className="form-control" id="firstName" placeholder="Your first name" />
+                        {errors.firstName?.type === "required" && <p className='mt-2 errormessage'>First Name can't be empty.</p>}
+                        {errors.firstName?.type === "maxLength" && <p className='mt-2 errormessage'>First name can't exceed 20 characters.</p>}
+                        {errors.firstName?.type === "pattern" && <p className='mt-2 errormessage'>Alphabetical characters only.</p>}
                     </div>
                     <div className="form-group">
-                        <label htmlFor="exampleFormControlInput1">Last Name</label>
-                        <input type="text" className="form-control" id="exampleFormControlInput1" placeholder="Your last name"/>
+                        <label htmlFor="lastName">Last Name</label>
+                        <input {...register("lastName", { required: true, maxLength: 20, pattern: /^[A-Za-z]+$/i })} type="text" className="form-control" id="exampleFormControlInput1" placeholder="Your last name" />
+                        {errors.lastName?.type === "required" && <p className='mt-2 errormessage'>Last Name can't be empty.</p>}
+                        {errors.lastName?.type === "maxLength" && <p className='mt-2 errormessage'>Last name can't exceed 20 characters.</p>}
+                        {errors.lastName?.type === "pattern" && <p className='mt-2 errormessage'>Alphabetical characters only.</p>}
                     </div>
                     <div className="form-group">
-                        <label htmlFor="exampleFormControlInput1">Email address</label>
-                        <input type="email" className="form-control" id="exampleFormControlInput1" placeholder="name@example.com"/>
+                        <label htmlFor="email">Email address</label>
+                        <input {...register("email", { required: true, pattern: /^[a-z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2,4}$/ })} type="text" className="form-control" id="exampleFormControlInput1" placeholder="name@example.com" />
+                        {errors.email?.type === "required" && <p className='mt-2 errormessage'>Email Name can't be empty.</p>}
+                        {errors.email?.type === "pattern" && <p className='mt-2 errormessage'>Invalid Email Address.</p>}
                     </div>
                     <div className="form-group">
-                        <label htmlFor="exampleFormControlInput1">Phone Number</label>
-                        <input type="phone" className="form-control" id="exampleFormControlInput1" placeholder="021 12345"/>
+                        <label htmlFor="phoneNumber">Phone Number</label>
+                        <input {...register("phoneNumber", { required: true, minLength: 6, maxLength: 12, pattern: /^[0-9]+$/ })} type="number" className="form-control" id="exampleFormControlInput1" placeholder="021 12345" />
+                        {errors.phoneNumber?.type === "required" && <p className='mt-2 errormessage'>Phone number can't be empty.</p>}
+                        {errors.phoneNumber?.type === "minLength" && <p className='mt-2 errormessage'>Phone number can't less than 6 characters.</p>}
+                        {errors.phoneNumber?.type === "maxLength" && <p className='mt-2 errormessage'>Phone number can't exceed 12 characters.</p>}
+                        {errors.phoneNumber?.type === "pattern" && <p className='mt-2 errormessage'>Numbers only.</p>}
                     </div>
                     <div className="form-group">
-                        <label htmlFor="exampleFormControlInput1">Date of Birth</label>
-                        <input type="date" className="form-control" id="exampleFormControlInput1"/>
+                        <label htmlFor="dateOfBirth">Date of Birth</label>
+                        <input {...register("dateOfBirth", { required: true })} type="date" className="form-control" id="exampleFormControlInput1" />
+                        {errors.dateOfBirth?.type === "required" && <p className='mt-2 errormessage'>You must choose Date of Birth.</p>}
                     </div>
                     <div className="form-group">
-                        <label htmlFor="exampleFormControlTextarea1">Comments</label>
+                        <label htmlFor="comments">Comments</label>
                         <textarea className="form-control" id="exampleFormControlTextarea1" rows="3" placeholder="Enter comments here"></textarea>
                     </div>
-                    <button id='submitbutton' className='btnn blue mr-3' type='submit'>Submit</button>
+                    <button id='submitbutton' className='btnn blue mr-2' type='submit'>Submit</button>
+                    <button id='resetbutton' className='btnn blue mr-2' type='reset'>Reset</button>
                     <Link to='/'><button className='btnn black'>Home</button></Link>
                 </form>
             </div>
-            
+
         </>
     )
 }
